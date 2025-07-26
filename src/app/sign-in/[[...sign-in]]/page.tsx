30a0df7e93e68aaf7d2ea7df3d2279d6e9f65a5a
Leo Wang <meimprankster@gmail.com>
2025-07-27 00:21:24 +0800

    better sign in page

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -1,21 +1,36 @@
 import { SignIn } from "@clerk/nextjs";
 import Image from "next/image";
+import { BackgroundBeams } from "~/components/ui/background-beam";
+import { Separator } from "~/components/ui/separator";
 
 export default function Page() {
 	return (
-		<main className="flex h-screen w-screen flex-col items-center justify-center gap-10">
-			<h1 className="font-bold font-serif text-4xl">我思故我在</h1>
-			<div className="-z-10 -translate-x-1/2 -translate-y-1/2 absolute top-1/2 left-1/2">
-				<Image
-					src="/rene-descartes.png"
-					alt="rene-descartes"
-					width={271}
-					height={290}
-				/>
-			</div>
-			<div className="opacity-50 transition-opacity duration-300 hover:opacity-100">
+		<main className="flex h-screen w-screen flex-col items-center justify-center gap-10 bg-slate-800 text-white">
+			<div className="z-20 flex flex-wrap items-center gap-5">
+				<div className="flex flex-col gap-5 p-10 font-serif">
+					<h1 className="font-bold font-serif text-4xl">思网</h1>
+					<h2 className="font-serif text-2xl">我思故我在</h2>
+					{[
+						{ icon: "💡", text: "探寻不同视角" },
+						{
+							icon: "✍️",
+							text: "提交你的回答，你的思维独一无二",
+						},
+						{
+							icon: "🚀",
+							text: "获得反馈，提升你的表达与思辨力",
+						},
+					].map(({ icon, text }) => (
+						<div key={text} className="flex items-center gap-3">
+							<span className="text-2xl">{icon}</span>
+							<span>{text}</span>
+						</div>
+					))}
+				</div>
+				<Separator orientation="vertical" />
 				<SignIn />
 			</div>
+			<BackgroundBeams className="z-0" />
 		</main>
 	);
 }
